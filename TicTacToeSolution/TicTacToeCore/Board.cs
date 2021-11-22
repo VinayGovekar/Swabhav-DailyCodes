7aaa30fd89fa5c0cfaae6790b2347434745fe353
L24999 <vinaygovekar24@gmail.com>
2021-11-22 21:54:34 +0530

    *30th Commit*
    * Updated TicTacToeApp - Added Board.cs

---
+++ TicTacToeSolution/TicTacToeCore/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToeCore
+{
+    public class Board
+    {
+        private Cell[] _boardCells = new Cell[9];
+        public Board()
+        {
+            for(int i = 0;i<_boardCells.Length;i++) _boardCells[i] = new Cell();
+        }
+        public bool CheckBoardFull()
+        {
+            foreach (var cell in _boardCells) if (cell.Mark == MarkType.EMPTY) return false;
+            return true;
+        }
+        public Cell[] BoardCells{ get { return _boardCells; } }
+    }
+}
