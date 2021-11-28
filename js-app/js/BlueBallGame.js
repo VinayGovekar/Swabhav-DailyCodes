5080bf1af8b32a105fb6ed8642d1cc792bc7f88f
L24999 <vinaygovekar24@gmail.com>
2021-11-28 21:59:53 +0530

    *42nd Commit*
    * Completed BlueBallGame

--- js-app/js/BlueBallGame.js
+++ js-app/js/BlueBallGame.js
@@ -1,13 +1,18 @@
 window.onload = (event) => {
-    onStart();
-    function onStart(){
-        var range =document.querySelector("#inputRange");
-        var value = document.querySelector(".value");
-        value.innerHTML="1";
-        range.addEventListener("input",changeValue)
-        function changeValue(){
-            value.innerHTML = range.value;
-        }
+    
+    var ballCount=1;
+    var secondContainer = document.querySelector(".secondContainer");
+    secondContainer.style.display = "none";
+    var range =document.querySelector("#inputRange");
+    var value = document.querySelector(".value");
+    value.innerHTML="1";
+    range.addEventListener("input",changeValue)
+    function changeValue(){
+        value.innerHTML = range.value;
     }
     
+    var firstBox = document.querySelector(".firstContainer");
+    function onStart(){
+
+    }
   };
